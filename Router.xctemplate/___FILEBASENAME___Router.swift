aae5fe638f1060a180f99be655cd69064f8b1530
john roque jorillo <jorillojohnroque@gmail.com>
2020-01-01 18:19:55 +0800

    Fix router bugs

--- Router.xctemplate/___FILEBASENAME___Router.swift
+++ Router.xctemplate/___FILEBASENAME___Router.swift
@@ -4,26 +4,26 @@ import Foundation
 import UIKit
 
 protocol ___FILEBASENAME___Delegate: AnyObject {
-	func dettach(_ coordinator: ___FILEBASENAME___)
+	func dettach(_ router: ___FILEBASENAME___)
 }
 
 
-class ___FILEBASENAME___: CoordinatorType {
-
+class ___FILEBASENAME___: RouterType {
+    
 	weak var delegate: ___FILEBASENAME___Delegate?
 
     var childRouters: [RouterType] = []
-     var navigation: UINavigationController
+    var navigation: UINavigationController
      
-     init(navigation: UINavigationController) {
-         self.navigation = navigation
-     }
-     
-     func start() {
-     }
-     
-     deinit {
-         print("--Deallocating \(self)")
-     }
+    init(navigation: UINavigationController) {
+        self.navigation = navigation
+    }
+    
+    func start() {
+    }
+    
+    deinit {
+        print("--Deallocating \(self)")
+    }
     
 }
